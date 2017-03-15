063340af0c1b814268f79b306ddf2079b7e7aa19
Benoit MARTIN <benoitmartin88.pro@gmail.com>
2017-03-15 21:44:26 +0100

    rpeo init. chrome with alpine 3.5 base

---
+++ chrome/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run --rm -it --net host \
+    -e DISPLAY=unix$DISPLAY \
+    -v ~/.Xauthority:/root/.Xauthority \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
+    -v /usr/share/fonts:/usr/share/fonts \
+    -v $HOME/.config/chromium/:/data \
+    -v $HOME/Downloads:/home/chromium/Downloads \
+    --device /dev/snd \
+    --device /dev/dri \
+    --name chromium bmartin/chromium
+
