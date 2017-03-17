15ff5b99587dd55bb41156e30c5d182cf259410f
Benoit MARTIN <benoitmartin88.pro@gmail.com>
2017-03-17 21:41:27 +0100

    install font inside container instead of mounting fonts dir from host

--- chromium/run.sh
+++ chromium/run.sh
@@ -2,11 +2,10 @@
 
 docker run --rm -it --net host \
     -e DISPLAY=unix$DISPLAY \
-    -e ALSA_CARD=1 \
+    -e ALSA_CARD=0 \
     -v ~/.Xauthority:/root/.Xauthority \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
-    -v /usr/share/fonts:/usr/share/fonts \
     -v $HOME/.config/chromium/:/data \
     -v $HOME/Downloads:/home/chromium/Downloads \
     --device /dev/snd \
