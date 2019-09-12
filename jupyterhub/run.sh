5c2629862462671a1da065920f70cdff8600ec34
Benoit Martin <benoitmartin88.pro@gmail.com>
2019-09-12 14:43:18 +0200

    update mount volume location

--- jupyterhub/run.sh
+++ jupyterhub/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker run --rm -it \
-  -v /Users/benoit.martin/docker/jupyterhub/data:/srv/jupyterhub \
-  -v /Users/benoit.martin/docker/jupyterhub/user_homes:/home \
-  -v /Users/benoit.martin/docker/jupyterhub/shared_data:/shared_data \
+  -v ${pwd}/data:/srv/jupyterhub \
+  -v ${pwd}/user_homes:/home \
+  -v ${pwd}/shared_data:/shared_data \
   -p 8000:8000 --name jupyterhub test_jupyterhub jupyterhub --config=/srv/jupyterhub/jupyterhub_config.py
 
